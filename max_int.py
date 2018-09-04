d340cca8e6f459f552ec1dabdd3791a5ddff9633
bjorksigur <bjorks18@ru.is>
2018-09-04 14:48:51 +0000

    first comment

---
+++ max_int.py
@@ -0,0 +1,15 @@
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+while num_int >= 0:
+    int(input("Input a number"))
+if num_int < 0: 
+    break
+
+print("The maximum is", max_int)    # Do not change this line
+
+
+#Fá notanda til þess að setja inn tölu.
+#Ef talan er stærri eða jafnt og 0 heldur notandi áfram að setja inn tölu.
+#Ef talan er minni en núll hættir kóðinn að keyra.
+#Prentar út hæðstu töluna sem notandinn sláir inn.
+
